6da73fa8ff35619266d2c181af153f9fa065f709
Necnion8 <necnionch@gmail.com>
2025-01-14 14:53:35 +0900

    initial commit

---
+++ src/main/java/com/gmail/necnionch/myplugin/dsrvunlinkslash/bukkit/UnlinkSlashPlugin.java
@@ -0,0 +1,17 @@
+package com.gmail.necnionch.myplugin.dsrvunlinkslash.bukkit;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class UnlinkSlashPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+}
